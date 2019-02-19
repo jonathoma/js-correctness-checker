09183917768037dded81fe84da9a73ab2d9dfe56
Jonathan Thomas <jonthoma@umich.edu>
2019-02-18 19:10:07 -0500

    Use forked dom-compare to get DOM differences

--- dom/compare_dom.js
+++ dom/compare_dom.js
@@ -1,11 +1,20 @@
 const request = require("request");
+
 const xmldom = require("xmldom");
-const parser = new xmldom.DOMParser();
-const domCompare = require("dom-compare");
+const xml_parser = new xmldom.DOMParser();
+const domCompare = require("../../dom-compare");
 const compare = domCompare.compare;
+
 const puppeteer = require('puppeteer');
+
+const fs = require('fs');
+
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const parser = require('html2hscript');
+
+const h = require('virtual-dom/h');
+const diff = require('virtual-dom/diff');
 
 async function get_dom_from_request(url) {
     request(url, function (error, response, body) {
@@ -35,47 +44,33 @@ async function get_dom_from_browser(url) {
     return html;
 }
 
+function get_dom_from_file(filename, strip=false) {
+    let text = fs.readFileSync(filename, 'utf8');
+    if (strip) {
+        text.replace('/<script([\S\s]*?)>([\S\s]*?)<\/script>/ig', '');
+    }
+    const dom = new JSDOM(text);
+    const { document } = dom.window;
+    return document;
+}
+
+function convert_html_to_hscript(text) {
+    let parsed = "";
+    parser(text, function(err, hscript) {
+        parsed = hscript;
+    });
+    return parsed;
+}
+
 function compare_simple_dom(dom_1, dom_2) {
-    var doc1 = parser.parseFromString(dom_1);
-    var doc2 = parser.parseFromString(dom_2);
+    var doc1 = xml_parser.parseFromString(dom_1);
+    var doc2 = xml_parser.parseFromString(dom_2);
     const result = compare(doc1, doc2);
     console.log(result);
     return result.getResult();
 }
 
-// async function get_dom_from_browser(url) {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-//     
-//     page.on('response', async (response) => {
-//         const status = response.status();
-//         console.log(status);
-//         //if ((status >= 300) && (status <= 399)) {
-//         //    console.log('Redirect from', 
-//         //                response.url(), 
-//         //                'to', 
-//         //                response.headers()['location']);
-//         //}
-//         text = await response.text();
-//         //console.log("This is the text: ", text);
-//         return text;
-//     });
-//     
-//     wait page.goto(url, {
-//         waitUntil: 'domcontentloaded'
-//     });
-//     
-//     setTimeout(async () => {
-//         console.log("Closing browser");
-//         await browser.close();
-//     }, 5000); // Wait for 5 seconds before closing browser.
-// }
- 
-function parse_dom(dom_text) {
-    return parser.parseFromString(dom_text);
-}
-
-function main() {
+function simple_dom_test() {
     // Comparison of simple DOM    
     const doc_1 = "<document> \
         <element attribute=\"10\" attributeX=\"100\"> \
@@ -109,28 +104,41 @@ function main() {
     console.log("Comparison of simple DOM example: ");
     console.log("Comparing identical: ", compare_simple_dom(doc_1, doc_2, { stripSpaces : true }), "\n");
     console.log("Comparing different: ", compare_simple_dom(doc_2, doc_3), "\n");
+}
 
-    // Comparison of real DOM
+function real_dom_test() {
     const url = "https://www.google.com";
     const source_dom_text = get_dom_from_request(url);
     const source_dom = new JSDOM(source_dom_text);
-    // console.log(source_dom);
 
-    // const source_dom_text_2 = get_dom_from_request(url);
-    // const source_dom_2 = new JSDOM(source_dom_text);
-    // console.log("Fetch same twice: ", source_dom === source_dom_2);
+    const source_dom_text_2 = get_dom_from_request(url);
+    const source_dom_2 = new JSDOM(source_dom_text);
+    console.log("Fetch same twice: ", source_dom === source_dom_2);
 
     const rendered_dom_text = get_dom_from_browser(url);
     const rendered_dom = new JSDOM(rendered_dom_text);
-    // console.log(rendered_dom);
+    console.log(rendered_dom);
 
-    // const rendered_dom_text_2 = get_dom_from_browser(url);
-    // const rendered_dom_2 = new JSDOM(rendered_dom_text);
-    // console.log("Load website same twice: ", rendered_dom === rendered_dom_2);
+    const rendered_dom_text_2 = get_dom_from_browser(url);
+    const rendered_dom_2 = new JSDOM(rendered_dom_text);
+    console.log("Load website same twice: ", rendered_dom === rendered_dom_2);
 
     console.log("\nComparison of real DOM example: "); 
     console.log(source_dom === rendered_dom);
 }
 
-main();
+function file_dom_test() {
+    let dom_1 = get_dom_from_file("samples/google_1.txt");
+    let dom_2 = get_dom_from_file("samples/google_2.txt");
+    let comp = compare(dom_1, dom_2);
+
+    console.log("%d / %d nodes differed. Diff score: %f", 
+        comp.getDifferences().length, comp.getTotal(), comp.getSummary());
+}
+
+(function main() {
+    // simple_dom_test();
+    // real_dom_test();
+    file_dom_test();
+})();
 
