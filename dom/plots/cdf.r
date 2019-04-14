34a3882b77d254d0815023cb811c47804bf6c7f5
Jonathan Thomas <jonthoma@umich.edu>
2019-04-14 14:55:31 -0400

    Add naive comparison plot

--- dom/plots/cdf.r
+++ dom/plots/cdf.r
@@ -11,7 +11,7 @@ ggplot(data, aes(x=data$score, colour=data$type)) +
     xlab("DOM difference ratio") +
     ylab("CDF across sites") +
     guides(fill=guide_legend(title="Config")) +
-    coord_cartesian(xlim=c(0, 0.5),ylim=c(0.3, 1)) +
+    coord_cartesian(xlim=c(0, 0.5),ylim=c(0, 1)) +
     scale_x_continuous(expand=c(0, 0)) +
     scale_y_continuous(expand=c(0, 0)) +
     scale_linetype_manual(values=c("solid","dotted","dashed")) +
@@ -40,7 +40,7 @@ ggplot(data, aes(x=data$score, colour=data$type)) +
     xlab("Network request difference ratio") +
     ylab("CDF across sites") +
     guides(fill=guide_legend(title="Config")) +
-    coord_cartesian(xlim=c(0, 1),ylim=c(0, 1)) +
+    coord_cartesian(xlim=c(0, 0.25),ylim=c(0, 1)) +
     scale_x_continuous(expand=c(0, 0)) +
     scale_y_continuous(expand=c(0, 0)) +
     scale_linetype_manual(values=c("solid","dotted","dashed")) +
@@ -69,7 +69,7 @@ ggplot(data, aes(x=data$score, colour=data$type)) +
     xlab("Screenshot difference ratio") +
     ylab("CDF across sites") +
     guides(fill=guide_legend(title="Config")) +
-    coord_cartesian(xlim=c(0, 1),ylim=c(0.3, 1)) +
+    coord_cartesian(xlim=c(0, 1),ylim=c(0, 1)) +
     scale_x_continuous(expand=c(0, 0)) +
     scale_y_continuous(expand=c(0, 0)) +
     scale_linetype_manual(values=c("solid","dotted","dashed")) +
@@ -98,7 +98,7 @@ ggplot(data, aes(x=data$score, colour=data$type)) +
     xlab("DOM difference ratio") +
     ylab("CDF across sites") +
     guides(fill=guide_legend(title="Config")) +
-    coord_cartesian(xlim=c(0, 0.5),ylim=c(0.3, 1)) +
+    coord_cartesian(xlim=c(0, 0.5),ylim=c(0, 1)) +
     scale_x_continuous(expand=c(0, 0)) +
     scale_y_continuous(expand=c(0, 0)) +
     scale_linetype_manual(values=c("solid","dotted","dashed")) +
@@ -119,3 +119,32 @@ ggplot(data, aes(x=data$score, colour=data$type)) +
         plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm"))
 .junk <- dev.off()
 
+
+output <- "naive_comparison_plot.png"
+data <- read.csv("../scores/naive_comparison.txt", header=TRUE, sep=",")
+png(output, height=2.5, width=5, units="in", res=300)
+ggplot(data, aes(x=data$score, colour=data$type)) +
+    stat_ecdf(pad=TRUE) +
+    xlab("DOM difference ratio") +
+    ylab("CDF across sites") +
+    guides(fill=guide_legend(title="Config")) +
+    coord_cartesian(xlim=c(0, 0.5),ylim=c(0, 1)) +
+    scale_x_continuous(expand=c(0, 0)) +
+    scale_y_continuous(expand=c(0, 0)) +
+    scale_linetype_manual(values=c("solid","dotted","dashed")) +
+    theme_bw() +
+    guides(fill = guide_legend(nrow = 1))+
+    theme(
+        legend.title=element_blank(),
+        legend.background=element_rect(color="darkgray", fill="white", linetype="solid", size=0.3),
+        legend.key=element_blank(),
+        legend.key.height=unit(12, "points"),
+        legend.key.width=unit(30, "points"),
+        legend.position=c(0.83, 0.30),
+        axis.title=element_text(size=15),
+        axis.text=element_text(size=14),
+        legend.text=element_text(size=8),
+        axis.title.y=element_text(margin=margin(0, 20, 0, 0)),
+        axis.title.x=element_text(margin=margin(10, 0, 0, 0)),
+        plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm"))
+.junk <- dev.off()
