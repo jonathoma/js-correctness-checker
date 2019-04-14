282fde5f835ba84d938bc1483d38dfca820eb0f4
Jonathan Thomas <jonthoma@umich.edu>
2019-04-14 14:54:07 -0400

    Refactor get_scores.py

--- dom/scores/get_scores.py
+++ dom/scores/get_scores.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
+
 def read_data(filename):
     data = {}
     with open(filename, 'r') as f:
@@ -11,84 +12,77 @@ def read_data(filename):
             if line.startswith('www'):
                 current_site = line.strip()
                 continue
-            differences = {}
-
-            # Get DOM tree diff information
-            l = line.strip().split()
-            # differences = {"diff_score": 0}
-            for label, val in list(zip(l[::2], l[1::2])):
-                differences[label] = val
-            differences["dom_diff_score"] = next(f).strip()
-
-            # Get network request diff information
-            line = f.readline()
-            l = line.strip().split()
-            # l = next(f).strip.split()
-            for label, val in list(zip(l[::2], l[1::2])):
-                differences[label] = val
-            differences["network_diff_score"] = next(f).strip()
-
-            # Get screenshot diff information
-            line = f.readline()
-            l = line.strip().split()
-            for label, val in list(zip(l[::2], l[1::2])):
-                differences[label] = val
-            differences["screenshot_diff_score"] = next(f).strip()
-
-            data[current_site] = differences
+            data[current_site] = get_differences(line, f)
     return data
 
+
+def get_differences(line, f):
+    differences = {}
+    differences["dom_diff_score"] = get_score(line)
+    line = f.readline()
+    differences["network_diff_score"] = get_score(line) 
+    line = f.readline()
+    differences["screenshot_diff_score"] = get_score(line) 
+    return differences
+
+
+def get_score(line):
+    l = line.strip().split()
+    num, diff = float(l[1]), float(l[3])
+    return diff / num
+
+
 def get_diff_scores(data):
-    # scores = [(site["dom_diff_score"], site["network_diff_score"], site["screenshot_diff_score"]) for site in data.values()]
-    dom_scores = [float(site["dom_diff_score"]) for site in data.values()]
+    dom_scores = [site["dom_diff_score"] for site in data.values()]
     dom_scores.sort()
-    network_scores = [float(site["network_diff_score"]) for site in data.values()]
+    network_scores = [site["network_diff_score"] for site in data.values()]
     network_scores.sort()
-    screenshot_scores = [float(site["screenshot_diff_score"]) for site in data.values()]
+    screenshot_scores = [site["screenshot_diff_score"] for site in data.values()]
     screenshot_scores.sort()
     return dom_scores, network_scores, screenshot_scores
+
+
+def write_file(outfile, live_scores, raw_scores, modified_scores, naive=False):
+    with open(outfile, 'w') as f:
+        f.write("type, score\n")
+        if naive:
+            write_naive_data(f, live_scores, raw_scores)
+        else:
+            write_data(f, live_scores, raw_scores, modified_scores)
+
+
+def write_data(f, live_scores, raw_scores, modified_scores):
+    for live in live_scores:
+        f.write("live, " + str(live) + "\n")
+    for raw in raw_scores:
+        f.write("replay, " + str(raw) + "\n")
+    for modified in modified_scores:
+        f.write("modified, " + str(modified) + "\n")
+
+
+def write_naive_data(f, live_scores, naive_scores):
+    for custom in live_scores:
+        f.write("custom, " + str(custom) + "\n")
+    for naive in naive_scores:
+        f.write("naive, " + str(naive) + "\n")
+
 
 def main():
     raw_data = read_data("raw_output.txt")
     modified_data = read_data("modified_output.txt")
     live_data = read_data("live_output.txt")
+
     raw_dom_scores, raw_network_scores, raw_screenshot_scores = get_diff_scores(raw_data)
     modified_dom_scores, modified_network_scores, modified_screenshot_scores = get_diff_scores(modified_data)
     live_dom_scores, live_network_scores, live_screenshot_scores = get_diff_scores(live_data)
-    with open("dom_scores.txt", 'w') as f:
-        f.write("type, score\n")
-        for live in live_dom_scores:
-            f.write("live, " + str(live) + "\n")
-        for raw in raw_dom_scores:
-            f.write("replay, " + str(raw) + "\n")
-        for modified in modified_dom_scores:
-            f.write("modified, " + str(modified) + "\n")
-    with open("network_scores.txt", 'w') as f:
-        f.write("type, score\n")
-        for live in live_network_scores:
-            f.write("live, " + str(live) + "\n")
-        for raw in raw_network_scores:
-            f.write("replay, " + str(raw) + "\n")
-        for modified in modified_network_scores:
-            f.write("modified, " + str(modified) + "\n")
-    with open("screenshot_scores.txt", 'w') as f:
-        f.write("type, score\n")
-        for live in live_screenshot_scores:
-            f.write("live, " + str(live) + "\n")
-        for raw in raw_screenshot_scores:
-            f.write("replay, " + str(raw) + "\n")
-        for modified in modified_screenshot_scores:
-            f.write("modified, " + str(modified) + "\n")
 
-    # Do naive vs our DOM comparison
+    write_file("dom_scores.txt", live_dom_scores, raw_dom_scores, modified_dom_scores)
+    write_file("network_scores.txt", live_network_scores, raw_network_scores, modified_network_scores)
+    write_file("screenshot_scores.txt", live_screenshot_scores, raw_screenshot_scores, modified_screenshot_scores)
+
     naive_data = read_data("naive_output.txt")
     naive_dom_scores, _, _ = get_diff_scores(naive_data)
-    with open("naive_comparison.txt", 'w') as f:
-        f.write("type, score\n")
-        for custom in live_dom_scores:
-            f.write("custom, " + str(custom) + "\n")
-        for naive in naive_dom_scores:
-            f.write("naive, " + str(naive) + "\n")
+    write_file("naive_comparison.txt", live_dom_scores, naive_dom_scores, [], naive=True)
 
 if __name__ == "__main__":
     main()
